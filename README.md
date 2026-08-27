1fd3cd44507a80ac84b17aa612d0b19b2c02a999
Vighnesh Gaikwad <vighneshgaikwad59@gmail.com>
2026-08-27 20:31:37 +0530

    Add files via upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# Max-Hours-Pass-Predictor
---
+++ logistic.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LogisticRegression
+x=[1],[2],[3],[4]
+y=[0,0,1,1]
+model=LogisticRegression()
+model.fit(x,y)
+
+hours=float(input("enter your number"))
+result =model.predict([[hours]])[0]
+
+if result==1:
+    print(f"based on hours{hours},you likely pass")
+else:
+    print(f"based on your result{hours}, you likey fail ")
