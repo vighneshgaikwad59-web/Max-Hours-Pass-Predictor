61b6c11bfe965bc8edb10981eae8a594e3eda03c
Vighnesh Gaikwad <vighneshgaikwad59@gmail.com>
2026-08-29 14:11:46 +0530

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -3,9 +3,10 @@ x=[1],[2],[3],[4]
 y=[0,0,1,1]
 model=LogisticRegression()
 model.fit(x,y)
-
+# Enter the hour value 
 hours=float(input("enter your number"))
 result =model.predict([[hours]])[0]
+
 
 if result==1:
     print(f"based on hours{hours},you likely pass")
